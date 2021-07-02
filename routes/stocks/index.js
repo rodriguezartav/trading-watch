8b5927517101165249c2ae1aa80265435d87e7a1
roberto <roberto@rodriguezartavia.com>
2021-07-01 20:24:15 -0600

    add stocks route

--- routes/stocks/index.js
+++ routes/stocks/index.js
@@ -8,31 +8,31 @@ const FinHub = require("../../helpers/finhub");
 
 const knex = Knex();
 
-router.post("/", async function (req, res, next) {
+router.all("/:symbol/:resolution", async function (req, res, next) {
   let start_date = req.body.start_date;
   if (!start_date) {
-    if (req.body.resolution == "1")
+    if (req.params.resolution == "1")
       start_date = moment().utcOffset(-4).add(-5, "hours").unix();
-    else if (req.body.resolution == "5")
+    else if (req.params.resolution == "5")
       start_date = moment().utcOffset(-4).startOf("day").unix();
-    else if (req.body.resolution == "15")
+    else if (req.params.resolution == "15")
       start_date = moment().utcOffset(-4).startOf("day").unix();
-    else if (req.body.resolution == "30")
+    else if (req.params.resolution == "30")
       start_date = moment().utcOffset(-4).add(-3, "days").unix();
-    else if (req.body.resolution == "60")
+    else if (req.params.resolution == "60")
       start_date = moment().utcOffset(-4).add(-7, "days").unix();
-    else if (req.body.resolution == "D")
+    else if (req.params.resolution == "D")
       start_date = moment().utcOffset(-4).add(-31, "months").unix();
-    else if (req.body.resolution == "W")
+    else if (req.params.resolution == "W")
       start_date = moment().utcOffset(-4).add(-3, "months").unix();
-    else if (req.body.resolution == "M")
+    else if (req.params.resolution == "M")
       start_date = moment().utcOffset(-4).add(-6, "months").unix();
   }
 
   const candles = await FinHub(
     "stockCandles",
-    req.body.symbol,
-    req.body.resolution,
+    req.params.symbol,
+    req.params.resolution,
     start_date,
     moment().unix(),
     {}
