aca5f59715e52668133dd321f62303ea480b5723
roberto <roberto@rodriguezartavia.com>
2021-07-07 11:31:51 -0600

    remove websockets

--- helpers/pusher.js
+++ helpers/pusher.js
@@ -1,11 +1,3 @@
 const Pusher = require("pusher");
 
-const pusher = new Pusher({
-  appId: "1231299",
-  key: "818630906bc836d5ce81",
-  secret: "da2ae05702ef08b42cfc",
-  cluster: "mt1",
-  useTLS: true,
-});
-
-module.exports = pusher;
+module.exports = Pusher;
